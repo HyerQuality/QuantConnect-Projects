70a3c97374eaafea8bd895b6bc9cdefc995a701d
Stephen Hyer <61244882+HyerQuality@users.noreply.github.com>
2022-02-03 20:27:55 -0500

    Delete QuantConnect directory

--- QuantConnect/Old Versions/Leveraged Indexing (Old)/DynamicWeights.py
+++
@@ -1,4 +0,0 @@
-##-------------------Class to hold variable weights---------------------------------------##        
-class Weights(Object):
-    MarginMultiplier = float(1.05)
-    ShortUVXY = float(0.805)
